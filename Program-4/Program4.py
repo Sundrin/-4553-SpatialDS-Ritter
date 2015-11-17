790657ec9815a08a09fbdf5fca375fdc315c2daf
Devin Ritter <ritterdevin@yahoo.com>
2015-11-17 14:42:29 -0600

    Update Program4.py

--- Program-4/Program4.py
+++ Program-4/Program4.py
@@ -1,1 +1,16 @@
+import csv
+import json
+
+nodes = []
+edges = []
+
+with open('nodes.csv', 'rb') as csvfile:
+  rows = csv.reader(csvfile, delimiter=',', quotechar="")
+  for row in rows:
+    nodes.append(row)
+
+with open('edges.csv', 'rb') as csvfile:
+  rows = csv.reader(csvfile, delimiter=',', quotechar="")
+  for row in rows:
+    nodes.append(row)
 
